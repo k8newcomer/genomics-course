ce273825dcf4f712a9bc35ef9d74c64ea0565b64
k8newcomer <44108256+k8newcomer@users.noreply.github.com>
2020-08-31 10:10:08 -0400

    Add files via upload

---
+++ lab1_kmn.R
@@ -0,0 +1,27 @@
+### Exercise 1
+x <- 3
+y <- 15
+x+y
+y-x
+x-y
+x*y
+x/y
+y/x
+
+### Exercise 2
+x <- 2
+y <- 3
+x^5 > y^4
+
+### Exercise 3
+x <- c(22,62,148,43,129)
+sum(x)
+22/sum(x)
+62/sum(x)
+148/sum(x)
+43/sum(x)
+129/sum(x)
+
+### Exercise 4
+x<- c("A","T","C","G")
+sort(x)
